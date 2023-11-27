2a3c6fe09089eeafa8c2569ce1196ff622134f1a
Adrian Aviles <adrian8167e@gmail.com>
2023-11-27 10:28:57 -0700

    Swagger customization
    Add Action argument to support swagger customization.

--- Flowsy.Web.Api/Documentation/DependencyInjection.cs
+++ Flowsy.Web.Api/Documentation/DependencyInjection.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Filters;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Flowsy.Web.Api.Documentation;
 
@@ -16,7 +18,24 @@ public static class DependencyInjection
     /// <param name="builder">The application builder</param>
     /// <param name="exampleAssemblies">The list of assemblies containing request examples.</param>
     /// <returns>The application builder.</returns>
-    public static WebApplicationBuilder AddDocumentation(this WebApplicationBuilder builder, params Assembly[] exampleAssemblies)
+    public static WebApplicationBuilder AddDocumentation(
+        this WebApplicationBuilder builder,
+        params Assembly[] exampleAssemblies
+        )
+        => builder.AddDocumentation(null, exampleAssemblies);
+
+    /// <summary>
+    /// Adds Swagger to the application builder. 
+    /// </summary>
+    /// <param name="builder">The application builder</param>
+    /// <param name="setupAction">An action to configure a SwaggerGenOptions instance.</param>
+    /// <param name="exampleAssemblies">The list of assemblies containing request examples.</param>
+    /// <returns>The application builder.</returns>
+    public static WebApplicationBuilder AddDocumentation(
+        this WebApplicationBuilder builder,
+        Action<SwaggerGenOptions>? setupAction = null,
+        params Assembly[] exampleAssemblies
+        )
     {
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -31,6 +50,8 @@ public static class DependencyInjection
             options.OperationFilter<ApiKeyOperationFilter>();
 
             options.ExampleFilters();
+            
+            setupAction?.Invoke(options);
         });
         builder.Services.AddSwaggerExamplesFromAssemblies(exampleAssemblies);
 
@@ -43,9 +64,18 @@ public static class DependencyInjection
     /// <param name="application">The application.</param>
     /// <returns>The application.</returns>
     public static WebApplication UseDocumentation(this WebApplication application)
+        => application.UseDocumentation(null);
+
+    /// <summary>
+    /// Configures Swagger in the application's request pipeline.
+    /// </summary>
+    /// <param name="application">The application.</param>
+    /// <param name="setupAction">An action to configure a SwaggerUIOptions instance.</param>
+    /// <returns>The application.</returns>
+    public static WebApplication UseDocumentation(this WebApplication application, Action<SwaggerUIOptions>? setupAction)
     {
         application.UseSwagger();
-        application.UseSwaggerUI();
+        application.UseSwaggerUI(options => setupAction?.Invoke(options));
         return application;
     }
 }
